ff84e9226323039f8f6acb1975eb00d21e42995a
SethKalkstein <sethkal7@yahoo.com>
2019-07-06 18:53:33 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>simple sessions practice</title>
+  </head>
+  <body>
+    <nav>
+      <ul>
+        <li> <a href="index.php">Page 1</a> </li>
+        <li> <a href="page2.php">Page 2</a> </li>
+        <li> <a href="page3.php">Page 3</a> </li>
+        <li> <a href="page4.php">Page 4</a> </li>
+      </ul>
+    </nav>
+  </body>
+</html>
