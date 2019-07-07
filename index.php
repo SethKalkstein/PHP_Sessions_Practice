be8c9912846ccdada9c9dcd834199dba6ff75ef2
SethKalkstein <sethkal7@yahoo.com>
2019-07-07 19:17:54 -0400

    added basic sessions functionality on index.php and page1.php

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
+  <?php
+    session_start();
+
+    if(isset($_POST["submit"])){
+      $_SESSION["name"] = $_POST["name"];
+    }
+   ?>
+
   <?php require "templates/header.php"; ?>
+
+  <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+    <input type="text" name="name">
+    <input type="submit" name="submit" value="submit">
+  </form>
+  <p>The sessions variable is set to <?php echo $_SESSION["name"]; ?></p>
   <?php require "templates/footer.php"; ?>
--- page2.php
+++ page2.php
@@ -1,2 +1,15 @@
+<?php session_start();
+
+?>
+
 <?php require "templates/header.php"; ?>
+
+<p>The sessions variable is set to <?php echo $_SESSION["name"] ?></p>
+
+<p>If you would like to apend more words to the end of the session variable, please do so below: </p>
+
+<form action="index.html" method="post">
+
+</form>
+
 <?php require "templates/footer.php"; ?>
