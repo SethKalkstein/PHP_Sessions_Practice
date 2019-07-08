cb2f3fb8697f78ec883b43d9adbe928c3cc78b7c
SethKalkstein <sethkal7@yahoo.com>
2019-07-07 20:19:51 -0400

    added functionalityto destroy the session variable

--- page4.php
+++ page4.php
@@ -1,10 +1,19 @@
 <?php
   session_start();
+
+  if(isset($_POST["destruction"])){
+    unset($_SESSION['name']);
+  }
 ?>
 
 
 <?php require "templates/header.php"; ?>
 
+<p>Please press delete if you would like to destroy the session variable! </p>
+
+<form action="<?php echo $_SYSTEM['PHP_SELF'] ?>" method="post">
+  <input type="submit" name="destruction" value="destroy">
+</form>
 
 
 <p>The sessions variable is set to:
